a9b830ee30811700d85bdeee0ae9d74e9b1de7c2
maekyngrigs10 <107376573+maekyngrigs10@users.noreply.github.com>
2022-07-04 09:56:28 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".f1").click(function(){
+    $(".f1f").show();
+	$(".f2").hide();
+    $(".f3").hide();
+    $(".f1").hide();
+});
+
+$(".f2").click(function(){
+    $(".f2f").show();
+	$(".f2").hide();
+    $(".f3").hide();
+    $(".f1").hide();
+});
+$(".f3").click(function(){
+    $(".f3f").show();
+	$(".f2").hide();
+    $(".f3").hide();
+    $(".f1").hide();
+});
+
+
